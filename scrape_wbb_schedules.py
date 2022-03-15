6f852447433fb2568893e19bde4d692682b33dab
saiemgilani <saiem.gilani@gmail.com>
2022-03-14 20:05:17 -0400

    wbb daily update

--- scrape_wbb_schedules.py
+++ scrape_wbb_schedules.py
@@ -3,33 +3,41 @@ import re
 import http
 import time
 import urllib.request
+import pandas as pd
+import sportsdataverse as sdv
 from urllib.error import URLError, HTTPError, ContentTooShortError
 from datetime import datetime
 from itertools import chain, starmap
-import pandas as pd
 from pathlib import Path
-from schedule_handler import ScheduleProcess
 path_to_schedules = "wbb/schedules/csv"
 final_file_name = "wbb_schedule_master.csv"
 
+def download_schedule(season, path_to_schedules=None):
+    df = sdv.wbb.espn_wbb_calendar(season)
+    calendar = df['dateURL'].tolist()
+    print(calendar)
+    ev = pd.DataFrame()
+    for d in calendar:
+        date_schedule = sdv.wbb.espn_wbb_schedule(dates=d)
+        ev = pd.concat([ev,date_schedule],axis=0, ignore_index=True)
+    ev = ev[ev['season_type'].isin([2,3])]
+    ev = ev.drop('competitors', axis=1)
+    ev = ev.drop_duplicates(subset=['game_id'], ignore_index=True)
+    if path_to_schedules is not None:
+        ev.to_csv(f"{path_to_schedules}/wbb_schedule_{season}.csv", index = False)
+    return ev
 def main():
 
     years_arr = range(2022,2023)
     schedule_table = pd.DataFrame()
     for year in years_arr:
-        print(year)
-        processor = ScheduleProcess(year)
-        year_schedule = processor.wbb_schedule()
-        year_schedule['game_id'] = year_schedule['id']
-         # this finds our json files
-        year_schedule.to_csv(f"{path_to_schedules}/wbb_schedule_{year}.csv", index=False)
-        schedule_table = schedule_table.append(year_schedule)
+        year_schedule = download_schedule(year, path_to_schedules)
+        schedule_table = pd.concat([schedule_table, year_schedule], axis=0)
     csv_files = [pos_csv.replace('.csv', '') for pos_csv in os.listdir(path_to_schedules) if pos_csv.endswith('.csv')]
     glued_data = pd.DataFrame()
     for index, js in enumerate(csv_files):
         x = pd.read_csv(f"{path_to_schedules}/{js}.csv", low_memory=False)
         glued_data = pd.concat([glued_data,x],axis=0)
-    glued_data['game_id'] = glued_data['id']
     glued_data.to_csv(final_file_name, index=False)
 
 if __name__ == "__main__":
